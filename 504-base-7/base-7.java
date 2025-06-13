3ec95b8bdc8c1805635294ec80ef72cb39094c79
MANISH KUMAR <165541196+ManishKumarCs@users.noreply.github.com>
2025-06-13 08:50:59 +0530

    Time: 1 ms (74.44%) | Memory: 41.2 MB (55.92%) - LeetSync

--- 504-base-7/base-7.java
+++ 504-base-7/base-7.java
@@ -1,6 +1,6 @@
 class Solution {
     public String convertToBase7(int num) {
-        if(num==0)return "0";
+    if(num==0)return "0";
      boolean neg = num<0;
      num=Math.abs(num);
      StringBuilder str = new StringBuilder();
