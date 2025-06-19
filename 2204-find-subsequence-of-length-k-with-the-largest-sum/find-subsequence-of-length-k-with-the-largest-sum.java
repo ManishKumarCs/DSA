35e69bf07fab7d18627f9ddde3bef1eb84bed73d
MANISH KUMAR <165541196+ManishKumarCs@users.noreply.github.com>
2025-06-19 13:09:58 +0530

    Time: 2 ms (100.00%) | Memory: 44.3 MB (92.80%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,16 +1,41 @@
 class Solution {
-    public int[] maxSubsequence(int[] nums, int k) {
-        List<int[]> list = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            list.add(new int[]{i, nums[i]});
+   public int[] maxSubsequence(int[] nums, int k) {
+        if (nums != null) {
+            int len = nums.length;
+            if (len > k && k > 0) {
+                int MAX = Integer.MIN_VALUE;
+                int MIN = Integer.MAX_VALUE;
+                for (int i : nums) {
+                    if (MIN > i)
+                        MIN = i;
+                    if (MAX < i)
+                        MAX = i;
+                }
+                int diff = MAX - MIN + 1;
+                int[] freqCount = new int[diff];
+                for (int num : nums) {
+                    freqCount[num - MIN]++;
+                }
+                int[] result = new int[k];
+                int tempLength = len;
+                for (int i = 0; tempLength > k; ) {
+                    if (freqCount[i] > 0) {
+                        freqCount[i]--;
+                        tempLength--;
+                    } else {
+                        i++;
+                    }
+                }
+                int index = 0;
+                for (int i = 0; i < len; i++) {
+                    if (freqCount[nums[i] - MIN] > 0) {
+                        result[index++] = nums[i];
+                        freqCount[nums[i] - MIN]--;
+                    }
+                }
+                nums = result;
+            }
         }
-        list.sort((a, b) -> Integer.compare(b[1], a[1]));
-        List<int[]> topK = list.subList(0, k);
-        topK.sort(Comparator.comparingInt(a -> a[0]));
-        int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
-            ans[i] = topK.get(i)[1];
-        }
-        return ans;
+        return nums;
     }
 }
