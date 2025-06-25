ebf47d4451f9fa4f78bd9843656c557232bb1742
MANISH KUMAR <165541196+ManishKumarCs@users.noreply.github.com>
2025-06-25 09:01:08 +0530

    Time: 80 ms (19.81%) | Memory: 45.4 MB (79.24%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.java
+++ 729-my-calendar-i/my-calendar-i.java
@@ -1,18 +1,17 @@
-class MyCalendar {
-    TreeMap<Integer, Integer> calendar;
+public class MyCalendar {
+    List<int[]> calendar;
 
     MyCalendar() {
-        calendar = new TreeMap();
+        calendar = new ArrayList();
     }
 
     public boolean book(int start, int end) {
-        Integer prev = calendar.floorKey(start),
-                next = calendar.ceilingKey(start);
-        if ((prev == null || calendar.get(prev) <= start) &&
-                (next == null || end <= next)) {
-            calendar.put(start, end);
-            return true;
+        for (int[] iv: calendar) {
+            if (iv[0] < end && start < iv[1]) {
+                return false;
+            }
         }
-        return false;
+        calendar.add(new int[]{start, end});
+        return true;
     }
 }
