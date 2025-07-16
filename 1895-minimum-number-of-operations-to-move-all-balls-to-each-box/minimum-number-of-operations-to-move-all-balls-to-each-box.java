2c67be12ef44d4770d8a49533ee7b0d32b130581
MANISH KUMAR <165541196+ManishKumarCs@users.noreply.github.com>
2025-07-16 09:14:42 +0530

    Time: 55 ms (52.36%) | Memory: 45.1 MB (25.41%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,18 +1,41 @@
+// class Solution {
+//     public int[] minOperations(String boxes) {
+//         int n = boxes.length();
+//         int[] result = new int[n];
+//         int j=0;
+//         for(char ch:boxes.toCharArray()){
+//             int a = Integer.parseInt(String.valueOf(ch));
+//             int count =0;
+//             for(int i=0;i<n;i++){
+//                 if(i==j)continue;
+//                 else if(boxes.charAt(i)=='1')count+=Math.abs(j-i);
+//             }
+//             result[j]=count;
+//             j++;
+//         }
+//         return result;
+//     }
+// }
+
 class Solution {
     public int[] minOperations(String boxes) {
-        int n = boxes.length();
-        int[] result = new int[n];
-        int j=0;
-        for(char ch:boxes.toCharArray()){
-            int a = Integer.parseInt(String.valueOf(ch));
-            int count =0;
-            for(int i=0;i<n;i++){
-                if(i==j)continue;
-                else if(boxes.charAt(i)=='1')count+=Math.abs(j-i);
-            }
-            result[j]=count;
-            j++;
+        List<Integer> li=new ArrayList<>();
+        int n=boxes.length();
+        int ans[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            char ch=boxes.charAt(i);
+            if(ch=='1') li.add(i);
         }
-        return result;
+        for(int i=0;i<n;i++)
+        {
+            int temp=0;
+            for(int j=0;j<li.size();j++)
+            {
+                temp+=Math.abs(li.get(j)-i);
+            }
+            ans[i]=temp;
+        }
+        return ans;
     }
 }
