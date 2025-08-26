0efd71edab3ba10a43d66341203d15699f0ab88e
MANISH KUMAR <165541196+ManishKumarCs@users.noreply.github.com>
2025-08-26 10:44:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (50.44%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(target==matrix[0][0])return true;
+        int i=0;
+        int j=matrix[0].length-1;
+        while(j>=0 && i<matrix.length){
+            if(target==matrix[i][j])return true;
+            else if(target<matrix[i][j])j--;
+            else if(target>matrix[i][j])i++; 
+        }
+        return false;
+    }
+}
